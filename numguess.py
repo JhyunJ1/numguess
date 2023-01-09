6de82330add730e8bc5d846a84d97c3154c6797e
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-09 16:16:03 +0900

    fix: Add function in numguess.py
    function:
    Input User's name
    Guess number

--- numguess.py
+++ numguess.py
@@ -2,3 +2,8 @@ from random import randint
 
 answer = randint(1, 100)
 print(f"answer is {answer}")
+
+username = input("What's your name? ")
+user_answer = int(input("Guess number > "))
+
+print(f"{username}'s answer is {user_answer}")
