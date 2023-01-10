bc9e0c33be54634c166fbe475db45265726c371f
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-10 15:07:29 +0900

    feat: Add hint feature to numguess.py

--- numguess.py
+++ numguess.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import isleep
 
 answer = randint(1, 100)
 print(f"answer is {answer}")
@@ -7,3 +8,17 @@ username = input("What's your name? ")
 user_answer = int(input("Guess number > "))
 
 print(f"{username}'s answer is {user_answer}")
+
+if answer == username:
+    print("****************")
+    sleep(1)
+    print("****************")
+    sleep(1)
+    print("****************")
+    sleep(1)
+    print(f"You got it right!! The answer is {answer}")
+elif guess > answer:
+    print(f"Keep going, man~! That was too high, {username}...")
+elif guess < answer:
+    print(f"Keep going, man~! That was too low, {username}...")
+
