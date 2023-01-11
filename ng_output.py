f588b2810446e828b4043cadb72023f669fb83db
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-11 17:00:29 +0900

    feat: modify ng_output.py
    Revice error

--- ng_output.py
+++ ng_output.py
@@ -9,8 +9,8 @@ def isRight(username: str, answer: int, user_answer: int):
             print(f"Do well {username}... Answer is lower than your answer.")
         return 0
 
-def GameOver(username:str,chance:int, user_chance:int):
-    if user_chance > chance:
-        print(f"Sorry, {username}... You lose")
+def GameOver(username:str):
+    print(f"Sorry, {username}... You lose")
+
 
 
