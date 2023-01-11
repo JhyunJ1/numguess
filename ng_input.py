07b2fef2ed546efd7606003037666701be2f1a9c
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-11 16:35:51 +0900

    feat: Create ng_input.py
    ng_input.py include input features

---
+++ ng_input.py
@@ -0,0 +1,8 @@
+def username():
+    return input("What's your name? ")
+
+def diff(username):
+    return input(f"Hello, {username}! Choose difficulty (easy, normal, hard)")
+
+def user_answer():
+    return input("Try >>> ")
