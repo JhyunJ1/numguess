1b7f36988e189d0f19d306cb38b29b6d52a6cc63
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-11 09:59:14 +0900

    feat: Debugging montyhall.py
    Using print function debug montyhall.py

--- montyhall.py
+++ montyhall.py
@@ -4,18 +4,18 @@ doors = [0,0,1]
 
 n = 100000
 
-nochange = 0
-change = 0
+nochange = 0.0
+change = 0.0
 
 for _ in range(100000):
     shuffle(doors)
     c = choice(doors)
-    doors.remove(0)
+    print(doors)
     if c == 1:
         nochange += 1
 
-float(nochange) /= n
-float(change) = 1.0 - nochagne
+nochange /= float(n)
+change = 1.0 - float(nochange)
 
 
 print(f"If, nothing change? {nochange}!!")
